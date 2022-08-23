01521aa4df9368a87e7cc6f982734ef912d31217
jdoug7280 <111395383+jdoug7280@users.noreply.github.com>
2022-08-23 10:49:32 -0400

    Created a new Home/Landing page. Combined index, about, and projects page into Home page. Minor UI enhancements and other fixes.

---
+++ script.js
@@ -0,0 +1,33 @@
+const navbar = document.querySelector(".navbar"); //Get all "navbar" classes on the page.
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section"); //Get all "section" tags on the page.
+const navbarLinks = document.querySelectorAll(".navbar-link");  //Get all "navbar-link" classes on the page.
+
+//Functions
+const mainFn = () => {
+    if (window.pageYOffset >= navbarOffsetTop) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+    sections.forEach((section, i) => { //Find auto-semicolon extension.
+        if(window.pageYOffset > section.offsetTop - 10){
+            navbarLinks.forEach((navbarLink) => {
+                navbarLink.classList.remove("change");
+            });
+            navbarLinks[i].classList.add("change");
+        }
+    });
+};
+
+mainFn();
+
+//Event Listeners
+window.addEventListener("scroll", () => {
+    mainFn();
+});
+
+window.addEventListener("resize", () => {
+    window.location.reload();
+});
